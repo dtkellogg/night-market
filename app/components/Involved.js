6014c9a7ebb54c14a276b31329b4b5f4660fc26b
Toshi <dtkellogg10@gmail.com>
2020-08-15 14:51:55 -0700

    Basic Skeleton Setup
    Built main component structure set up Routes, basic Nav

---
+++ app/components/Involved.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Involved() {
+    return (
+        <h1>Get Involved</h1>
+    )
+}
--- app/index.js
+++ app/index.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import './index.css'
+import Nav from "./components/Nav"
+import About from "./components/About"
+import Home from "./components/Home"
+import Team from "./components/Team"
+import Contact from "./components/Contact"
+import Involved from "./components/Involved"
 
 function App () {
-    return(
-        <div>
-            Hello World
+    return (
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/team">
+              <Team />
+            </Route>
+            <Route path="/involved">
+              <Involved />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
         </div>
-    )
+      </Router>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
